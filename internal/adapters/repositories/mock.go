935971a327be02117a80d8b59bf7c242056ba417
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-23 23:05:15 -0300

    Adds mock for repositories.

---
+++ internal/adapters/repositories/mock.go
@@ -0,0 +1,16 @@
+package repositories
+
+import "github.com/ariel17/jobberwocky/internal/core/domain"
+
+type MockRepository struct {
+	Jobs  []domain.Job
+	Error error
+}
+
+func (m *MockRepository) Filter() ([]domain.Job, error) {
+	return m.Jobs, m.Error
+}
+
+func (m *MockRepository) Save(_ domain.Job) error {
+	return m.Error
+}
