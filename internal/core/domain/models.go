9d15819858b7e47d8ecafe374fcb4d35c35f3ebf
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-24 16:43:25 -0300

    Adds job type validation.

--- internal/core/domain/models.go
+++ internal/core/domain/models.go
@@ -1,16 +1,32 @@
 package domain
 
+const (
+	FullTime   = "Full-Time"
+	Contractor = "Contractor"
+	PartTime   = "Part-Time"
+)
+
 // Job contains the details of a hiring position on a company.
 type Job struct {
 	Title            string
 	Description      string
 	Company          string
 	Location         string
-	SalaryMin        *int
+	SalaryMin        int
 	SalaryMax        int
 	Type             string
 	IsRemoteFriendly bool
 	Keywords         []string
+}
+
+// IsTypeValid checks that `Type` field only contains specific values.
+func (j Job) IsTypeValid() bool {
+	for _, t := range []string{Contractor, FullTime, PartTime} {
+		if j.Type == t {
+			return true
+		}
+	}
+	return false
 }
 
 // Filter contains value patterns to match when searching for matching jobs.
