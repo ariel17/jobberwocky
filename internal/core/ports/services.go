5bb9f435adbe21b5672e2142a94c3a4f937954c9
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-23 23:33:57 -0300

    Changes parameter type.

--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -5,7 +5,7 @@ import "github.com/ariel17/jobberwocky/internal/core/domain"
 // JobService is responsible for storing new services and retrieve all those
 // that match a given pattern, if present.
 type JobService interface {
-	Match(pattern domain.Filter) ([]domain.Job, error)
+	Match(pattern *domain.Filter) ([]domain.Job, error)
 	Create(job domain.Job) error
 }
 
