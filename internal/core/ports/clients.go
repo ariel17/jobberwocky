75fc93a3dfcd2d908ecb43615fb2cde7748a9648
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-24 18:56:50 -0300

    Adds email provider client interface.

--- internal/core/ports/clients.go
+++ internal/core/ports/clients.go
@@ -5,3 +5,7 @@ import "github.com/ariel17/jobberwocky/internal/core/domain"
 type ExternalJobClient interface {
 	Match() ([]domain.Job, error)
 }
+
+type EmailProviderClient interface {
+	Send(from, to, subject, body string) error
+}
