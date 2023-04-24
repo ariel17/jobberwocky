35ffd193847b03feff117035eabd3b939b5913f2
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-24 00:50:15 -0300

    Adds mock implementation for filter method.

--- internal/adapters/repositories/mock.go
+++ internal/adapters/repositories/mock.go
@@ -1,16 +1,50 @@
 package repositories
 
-import "github.com/ariel17/jobberwocky/internal/core/domain"
+import (
+	"strings"
 
-type MockRepository struct {
+	"github.com/ariel17/jobberwocky/internal/core/domain"
+)
+
+type MockJobRepository struct {
 	Jobs  []domain.Job
 	Error error
 }
 
-func (m *MockRepository) Filter(pattern domain.Filter) ([]domain.Job, error) {
-	return m.Jobs, m.Error
+func (m *MockJobRepository) Filter(pattern *domain.Filter) ([]domain.Job, error) {
+	if m.Error != nil {
+		return nil, m.Error
+	}
+	if pattern == nil {
+		return m.Jobs, m.Error
+	}
+	results := []domain.Job{}
+	for _, job := range m.Jobs {
+		if (pattern.Text != "" && strings.Contains(job.Title+job.Description, pattern.Text)) ||
+			(pattern.Location != "" && pattern.Location == job.Location) ||
+			(pattern.Salary > 0 && pattern.Salary >= job.SalaryMin && pattern.Salary <= job.SalaryMax) ||
+			(pattern.Type != "" && pattern.Type == job.Type) ||
+			(pattern.IsRemoteFriendly != nil && *pattern.IsRemoteFriendly == job.IsRemoteFriendly) {
+			results = append(results, job)
+			continue
+		}
+		for _, patternKeyword := range pattern.Keywords {
+			found := false
+			for _, jobKeyword := range job.Keywords {
+				if patternKeyword == jobKeyword {
+					found = true
+					break
+				}
+			}
+			if found {
+				results = append(results, job)
+				break
+			}
+		}
+	}
+	return results, m.Error
 }
 
-func (m *MockRepository) Save(_ domain.Job) error {
+func (m *MockJobRepository) Save(_ domain.Job) error {
 	return m.Error
 }
