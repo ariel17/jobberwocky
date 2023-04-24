7d9814616b3b6b1b1e9123898ab1319a84226ff9
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-23 23:15:43 -0300

    Modifies return values on interface.

--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -2,11 +2,15 @@ package ports
 
 import "github.com/ariel17/jobberwocky/internal/core/domain"
 
+// JobService is responsible for storing new services and retrieve all those
+// that match a given pattern, if present.
 type JobService interface {
-	Match() []domain.Job
+	Match() ([]domain.Job, error)
 	Create(domain.Job) error
 }
 
+// NotificationService sends job alerts when its details matches subscribers'
+// patterns.
 type NotificationService interface {
 	Send(domain.Job) error
 }
