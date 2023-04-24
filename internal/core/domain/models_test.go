3eda61d2cdc4d0b2d66b3a74741ce0c56f51feaf
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-24 16:48:44 -0300

    Simplifies test cases.

--- internal/core/domain/models_test.go
+++ internal/core/domain/models_test.go
@@ -8,18 +8,17 @@ import (
 
 func TestJob_IsTypeValid(t *testing.T) {
 	testCases := []struct {
-		name    string
 		jobType string
 		isValid bool
 	}{
-		{"Contractor", Contractor, true},
-		{"Full-Time", Contractor, true},
-		{"Part-Time", Contractor, true},
-		{"other value", "other value", false},
+		{Contractor, true},
+		{FullTime, true},
+		{PartTime, true},
+		{"other value", false},
 	}
 
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
+		t.Run(tc.jobType, func(t *testing.T) {
 			job := Job{Type: tc.jobType}
 			assert.Equal(t, tc.isValid, job.IsTypeValid())
 		})
