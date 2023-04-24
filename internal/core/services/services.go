ee3e7d1fd48f3250c2c3a13f4d9d2b7903672dec
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-23 23:16:04 -0300

    Implements minimal code to pass creation tests.

--- internal/core/services/services.go
+++ internal/core/services/services.go
@@ -1,1 +1,24 @@
 package services
+
+import (
+	"github.com/ariel17/jobberwocky/internal/core/domain"
+	"github.com/ariel17/jobberwocky/internal/core/ports"
+)
+
+type jobService struct {
+	repository ports.JobRepository
+}
+
+func NewJobService(repository ports.JobRepository) ports.JobService {
+	return &jobService{
+		repository: repository,
+	}
+}
+
+func (j *jobService) Match() ([]domain.Job, error) {
+	return nil, nil
+}
+
+func (j *jobService) Create(job domain.Job) error {
+	return j.repository.Save(job)
+}
