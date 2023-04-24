9d5afb83905770a63a18c6e4a98adb80d341d073
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-24 00:50:51 -0300

    Changes parameter type on repository.

--- internal/core/ports/repositories.go
+++ internal/core/ports/repositories.go
@@ -3,6 +3,6 @@ package ports
 import "github.com/ariel17/jobberwocky/internal/core/domain"
 
 type JobRepository interface {
-	Filter(pattern domain.Filter) ([]domain.Job, error)
+	Filter(pattern *domain.Filter) ([]domain.Job, error)
 	Save(job domain.Job) error
 }
