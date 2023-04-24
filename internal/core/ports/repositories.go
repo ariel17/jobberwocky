62d3e7ed955da62c8705a00499f53efd5f563ccb
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-24 19:17:02 -0300

    Adds subscription repository interface.

--- internal/core/ports/repositories.go
+++ internal/core/ports/repositories.go
@@ -6,3 +6,8 @@ type JobRepository interface {
 	Filter(pattern *domain.Filter) ([]domain.Job, error)
 	Save(job domain.Job) error
 }
+
+type SubscriptionRepository interface {
+	Filter(job domain.Job) ([]domain.Subscription, error)
+	Save(subscription domain.Subscription) error
+}
