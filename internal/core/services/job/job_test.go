cc9dc74fcd60a24cac0253335f0f60581d7e9300
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-25 00:15:37 -0300

    Adds external source client to filter method on job service.

--- internal/core/services/job/job_test.go
+++ internal/core/services/job/job_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/ariel17/jobberwocky/internal/adapters/clients"
 	"github.com/ariel17/jobberwocky/internal/adapters/repositories"
 	"github.com/ariel17/jobberwocky/internal/core/domain"
 	"github.com/ariel17/jobberwocky/internal/core/services/notification"
@@ -40,33 +41,38 @@ func TestJobService_Create(t *testing.T) {
 
 func TestJobService_Match(t *testing.T) {
 	testCases := []struct {
-		name    string
-		pattern *domain.Pattern
-		err     error
-		matches int
+		name          string
+		pattern       *domain.Pattern
+		repositoryErr error
+		externalErr   error
+		matches       int
 	}{
-		{"all without filter", nil, nil, 3},
-		{"all with empty filter", &domain.Pattern{}, nil, 3},
-		{"Pattern by title text", &domain.Pattern{Text: "technical"}, nil, 1},
-		{"Pattern by description text", &domain.Pattern{Text: "you"}, nil, 1},
-		{"Pattern by salary in range", &domain.Pattern{Salary: 7000}, nil, 1},
-		{"Pattern by ranged/fixed salary", &domain.Pattern{Salary: 8000}, nil, 3},
-		{"Pattern by company", &domain.Pattern{Company: "IBM"}, nil, 1},
-		{"Pattern by location", &domain.Pattern{Location: "USA"}, nil, 1},
-		{"Pattern by type", &domain.Pattern{Type: domain.Contractor}, nil, 1},
-		{"Pattern by remote friendly", &domain.Pattern{IsRemoteFriendly: boolPointer(true)}, nil, 2},
-		{"Pattern by single keyword", &domain.Pattern{Keywords: []string{"sql"}}, nil, 1},
-		{"Pattern by multiple keywords that does not match", &domain.Pattern{Keywords: []string{"sql", "java"}}, nil, 0},
-		{"Pattern by multiple keywords that matches", &domain.Pattern{Keywords: []string{"golang", "java"}}, nil, 1},
-		{"Pattern by keywords and text", &domain.Pattern{Text: "technical", Keywords: []string{"sql", "java"}}, nil, 0},
-		{"failed by repository error", nil, errors.New("mocked error"), 0},
+		// only local source
+		{"all without filter", nil, nil, nil, 3},
+		{"all with empty filter", &domain.Pattern{}, nil, nil, 3},
+		{"pattern by title text", &domain.Pattern{Text: "technical"}, nil, nil, 1},
+		{"pattern by description text", &domain.Pattern{Text: "you"}, nil, nil, 1},
+		{"pattern by salary in range", &domain.Pattern{Salary: 7000}, nil, nil, 1},
+		{"pattern by ranged/fixed salary", &domain.Pattern{Salary: 8000}, nil, nil, 3},
+		{"pattern by company", &domain.Pattern{Company: "IBM"}, nil, nil, 1},
+		{"pattern by location", &domain.Pattern{Location: "USA"}, nil, nil, 1},
+		{"pattern by type", &domain.Pattern{Type: domain.Contractor}, nil, nil, 1},
+		{"pattern by remote friendly", &domain.Pattern{IsRemoteFriendly: boolPointer(true)}, nil, nil, 2},
+		{"pattern by single keyword", &domain.Pattern{Keywords: []string{"sql"}}, nil, nil, 1},
+		{"pattern by multiple keywords that does not match", &domain.Pattern{Keywords: []string{"sql", "java"}}, nil, nil, 0},
+		{"pattern by multiple keywords that matches", &domain.Pattern{Keywords: []string{"golang", "java"}}, nil, nil, 1},
+		{"pattern by keywords and text", &domain.Pattern{Text: "technical", Keywords: []string{"sql", "java"}}, nil, nil, 0},
+		{"failed by repository error", nil, errors.New("mocked error"), nil, 0},
+
+		// only external sources
+		{"pattern by text matches external", &domain.Pattern{Text: "external"}, nil, nil, 1},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			repository := repositories.MockJobRepository{
 				MockFilter: repositories.MockFilter{
-					Error: tc.err,
+					Error: tc.repositoryErr,
 					Jobs: []domain.Job{
 						{"Looking for a Technical Leader", "Very long description.", "Ariel Labs", "Argentina", 6000, 8000, domain.FullTime, true, []string{"golang", "java", "python", "mysql"}},
 						{"Sr Java developer", "We need you.", "IBM", "USA", 0, 8000, domain.PartTime, false, []string{"java"}},
@@ -74,10 +80,18 @@ func TestJobService_Match(t *testing.T) {
 					},
 				},
 			}
-			service := NewJobService(&repository, nil)
+			external := clients.MockExternalJobClient{
+				MockFilter: repositories.MockFilter{
+					Error: nil,
+					Jobs: []domain.Job{
+						{"External", "", "", "Argentina", 0, 8000, "", nil, []string{"sql"}},
+					},
+				},
+			}
+			service := NewJobService(&repository, nil, &external)
 			jobs, err := service.Filter(tc.pattern)
 			assert.True(t, repository.FilterWasCalled())
-			assert.Equal(t, tc.err, err)
+			assert.Equal(t, tc.repositoryErr, err)
 			if err == nil {
 				assert.NotNil(t, jobs)
 				assert.Equal(t, tc.matches, len(jobs))
