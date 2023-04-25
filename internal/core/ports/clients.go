fefa7c2aa5e29a397f3fca1677f2b1b209700efd
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-24 23:12:34 -0300

    Adds filter paramter for external clients.

--- internal/core/ports/clients.go
+++ internal/core/ports/clients.go
@@ -3,7 +3,7 @@ package ports
 import "github.com/ariel17/jobberwocky/internal/core/domain"
 
 type ExternalJobClient interface {
-	Match() ([]domain.Job, error)
+	Match(pattern domain.Filter) ([]domain.Job, error)
 }
 
 type EmailProviderClient interface {
