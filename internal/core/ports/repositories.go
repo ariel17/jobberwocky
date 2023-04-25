aeb16af920ec1cf19a1e0d8e103ad8e227f36d0c
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-25 20:27:38 -0300

    Adds method to synchronize schemas into repositories.

--- internal/core/ports/repositories.go
+++ internal/core/ports/repositories.go
@@ -2,12 +2,18 @@ package ports
 
 import "github.com/ariel17/jobberwocky/internal/core/domain"
 
+type Repository interface {
+	SyncSchemas() error
+}
+
 type JobRepository interface {
+	Repository
 	JobFilter
 	Save(job domain.Job) error
 }
 
 type SubscriptionRepository interface {
+	Repository
 	Filter(job domain.Job) ([]domain.Subscription, error)
 	Save(subscription domain.Subscription) error
 }
