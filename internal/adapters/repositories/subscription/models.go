7f867d430769a95dfd7d3a07c631990fa1e172a6
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-26 02:25:57 -0300

    Adds subscription models.

---
+++ internal/adapters/repositories/subscription/models.go
@@ -0,0 +1,31 @@
+package subscription
+
+import (
+	"github.com/ariel17/jobberwocky/internal/adapters/repositories"
+	"github.com/ariel17/jobberwocky/internal/core/domain"
+)
+
+type Subscription struct {
+	ID               int64  `gorm:"primaryKey"`
+	Email            string `gorm:"size:50;not null;uniqueIndex"`
+	Text             string `gorm:"size:20"`
+	Company          string `gorm:"size:50"`
+	Location         string `gorm:"size:20"`
+	Salary           int
+	Type             string `gorm:"size:10"`
+	IsRemoteFriendly *bool
+	Keywords         []repositories.Keyword `gorm:"many2many:subscriptions_keywords;"`
+}
+
+func subscriptionDomainToModel(s domain.Subscription) Subscription {
+	return Subscription{
+		Email:            s.Email,
+		Text:             s.Text,
+		Company:          s.Company,
+		Location:         s.Location,
+		Salary:           s.Salary,
+		Type:             s.Type,
+		IsRemoteFriendly: s.IsRemoteFriendly,
+		Keywords:         repositories.StringKeywordsToModel(s.Keywords),
+	}
+}
