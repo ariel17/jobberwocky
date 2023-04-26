5e56ef501c1dbab979db2ba91a49db10665ef0be
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-26 04:51:11 -0300

    Adds test cases for filtering.

--- internal/adapters/repositories/subscription/subscription_test.go
+++ internal/adapters/repositories/subscription/subscription_test.go
@@ -9,6 +9,7 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 
+	"github.com/ariel17/jobberwocky/internal/adapters/repositories/job"
 	"github.com/ariel17/jobberwocky/internal/adapters/repositories/keyword"
 	"github.com/ariel17/jobberwocky/internal/core/domain"
 	helpers "github.com/ariel17/jobberwocky/internal/internal_test"
@@ -60,6 +61,57 @@ func TestSubscriptionRepository_Save(t *testing.T) {
 				}
 				assert.True(t, found)
 			}
+		})
+	}
+}
+
+func TestSubscriptionRepository_Filter(t *testing.T) {
+	subscriptions := []domain.Subscription{
+		{Pattern: domain.Pattern{}, Email: "user1@example.com"},
+		{Pattern: domain.Pattern{Text: "unique"}, Email: "user2@example.com"},
+		{Pattern: domain.Pattern{Text: "wow!"}, Email: "user3@example.com"},
+		{Pattern: domain.Pattern{Company: "X"}, Email: "user4@example.com"},
+		{Pattern: domain.Pattern{Type: domain.FullTime}, Email: "user5@example.com"},
+		{Pattern: domain.Pattern{Keywords: []string{"k1"}}, Email: "user6@example.com"},
+		{Pattern: domain.Pattern{Keywords: []string{"k5"}}, Email: "user7@example.com"},
+	}
+	testCases := []struct {
+		name     string
+		job      domain.Job
+		expected int
+	}{
+		{"matches by empty filter and type and keywords", domain.Job{"This is the title", "The description", "Company1", "Argentina", 60, 80, domain.FullTime, helpers.BoolPointer(true), []string{"k1", "k2", "k3"}, ""}, 3},
+		{"matches by empty filter and text in title and keywords", domain.Job{"Unique title", "The description", "Company2", "Argentina", 60, 80, domain.FullTime, helpers.BoolPointer(true), []string{"k1", "k2", "k3"}, ""}, 3},
+		{"matches by empty filter and text in description and keywords", domain.Job{"title1", "Wow!", "Company3", "Argentina", 60, 80, domain.FullTime, helpers.BoolPointer(true), []string{"k1", "k2", "k3"}, ""}, 3},
+		{"matches by empty filter and company and type and keywords", domain.Job{"title2", "Wow!", "X", "Argentina", 60, 80, domain.FullTime, helpers.BoolPointer(true), []string{"k1", "k2", "k3"}, ""}, 4},
+		{"matches by only keywords", domain.Job{"title2", "Wow!", "X", "Argentina", 60, 80, domain.FullTime, helpers.BoolPointer(true), []string{"k5"}, ""}, 4},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_ = os.Remove(dbName)
+			db, err := gorm.Open(sqlite.Open(dbName), &gorm.Config{
+				Logger: logger.Default.LogMode(logger.Info),
+			})
+			assert.Nil(t, err)
+
+			keywordRepository := keyword.NewKeywordRepository(db)
+			assert.Nil(t, keywordRepository.SyncSchemas())
+
+			jobRepository := job.NewJobRepository(db)
+			assert.Nil(t, jobRepository.SyncSchemas())
+			assert.Nil(t, jobRepository.Save(tc.job))
+
+			repository := NewSubscriptionRepository(db)
+			assert.Nil(t, repository.SyncSchemas())
+
+			for _, s := range subscriptions {
+				assert.Nil(t, repository.Save(s))
+			}
+
+			matchingSubscriptions, err := repository.Filter(tc.job)
+			assert.Nil(t, err)
+			assert.NotNil(t, matchingSubscriptions)
+			assert.Equal(t, tc.expected, len(matchingSubscriptions))
 		})
 	}
 }
