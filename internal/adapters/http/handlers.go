f842e7a7113eba1f7ba413c9c98592d730ec18e3
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2023-04-27 05:29:57 -0300

    Adds Swagger handlers configuration.

--- internal/adapters/http/handlers.go
+++ internal/adapters/http/handlers.go
@@ -1,7 +1,27 @@
 package http
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+
+	"github.com/ariel17/jobberwocky/api"
+	"github.com/ariel17/jobberwocky/resources/configs"
+)
 
 type Handler interface {
 	ConfigureRoutes(router *gin.Engine)
+}
+
+type swaggerHandler struct{}
+
+func NewSwaggerHandler() Handler {
+	return &swaggerHandler{}
+}
+
+func (s *swaggerHandler) ConfigureRoutes(router *gin.Engine) {
+	api.SwaggerInfo.Host = fmt.Sprintf("localhost:%s", configs.GetHTTPPort())
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
